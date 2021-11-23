e6ea882023271d233d70238bd0066ae019ac0a17
Andrew Gordon <andgordo@ucsc.edu>
2021-11-23 08:40:22 -0800

    Adding columns, mixed with row example

--- src/flutter_src/lib/main.dart
+++ src/flutter_src/lib/main.dart
@@ -23,21 +23,36 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color(0xff66b366),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Horizontal Spacing
-        crossAxisAlignment: CrossAxisAlignment.start, // Vertical Spacing
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Text('Hello, World'),
-          FlatButton(
-            onPressed: () {},
-            color: Colors.black26,
-            child: Text('Click Me'),
+          Row(
+            children: <Widget>[
+              Text('Hello'),
+              Text(' World'),
+            ]
           ),
           Container(
+            padding: EdgeInsets.all(20.0),
             color: Colors.cyan,
-            padding: EdgeInsets.all(30.0),
-            child: Text('Inside container'),
-          )
+            child: Text('One'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.pink,
+            child: Text('Two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.amber,
+            child: Text('Three'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.red,
+            child: Text('Four'),
+          ),
         ]
       ),
       floatingActionButton: const FloatingActionButton(
